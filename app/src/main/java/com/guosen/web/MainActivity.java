ce531cf3f8aeb04119eb8498657ced6cc2a94188
q1770750695 <q1770750695@gmail.com>
2019-05-23 17:30:16 +0800

    deviceid

--- app/src/main/java/com/guosen/web/MainActivity.java
+++ app/src/main/java/com/guosen/web/MainActivity.java
@@ -1,9 +1,14 @@
 package com.guosen.web;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -14,6 +19,7 @@ import android.webkit.WebView;
 
 import java.net.NetworkInterface;
 import java.security.MessageDigest;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -36,13 +42,12 @@ public class MainActivity extends AppCompatActivity {
         settings.setAllowFileAccess(true);
         settings.setSupportZoom(true);
         settings.setDatabaseEnabled(true);
-        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
         _webView.clearCache(true);
         _webView.addJavascriptInterface(this, "native");
-        //_webView.loadUrl("http://www.o6o4.com");
+        _webView.loadUrl("http://www.o6o4.com");
         //_webView.loadUrl("http://125.88.183.165:8082");
-        _webView.loadUrl("http://172.24.177.42:8099");
-        Log.e("id", getDeviceID());//DC-55-83-E3-0B-A5
+        //_webView.loadUrl("http://172.24.177.42:8099");
     }
     public String digest(String src) throws Exception {
         if (null == src || src.length() == 0) return null;
@@ -51,10 +56,34 @@ public class MainActivity extends AppCompatActivity {
         bytes = digest.digest(bytes);
         return new String(bytes, "ASCII");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String permissions[] = {
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_WIFI_STATE
+        };
+        ArrayList<String> toApplyList = new ArrayList<>();
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);// 进入到这里代表没有权限.
+            }
+        }
+        String tmpList[] = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
+        //Log.e("id", getDeviceID());
+    }
+
     @JavascriptInterface
     public String getDeviceID() {
         SharedPreferences sp = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
-        String pdaID = sp.getString("webid", null);
+        String tmpID, pdaID = null;//sp.getString("webid", null);
         if (null != pdaID) return pdaID;
         try {
             List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
@@ -65,16 +94,28 @@ public class MainActivity extends AppCompatActivity {
                 StringBuilder res1 = new StringBuilder();
                 for (byte b : macBytes) res1.append(String.format("%02X:", b));
                 if (res1.length() != 0) res1.deleteCharAt(res1.length() - 1);
-                pdaID = res1.toString();
+                pdaID = res1.toString().replaceAll(":", "");
+                Log.e("pdaIDmac." + nif.getName(), pdaID);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        if (null == pdaID) {
-            pdaID = UUID.randomUUID().toString().replaceAll("-", "");
+        tmpID = ((TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+        if (null == tmpID) {
+            tmpID = ((TelephonyManager) getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE)).getSimSerialNumber();
+            if (null != tmpID) {
+                Log.e("tmpID.SerialNumber", tmpID);
+            }
         } else {
-            pdaID = pdaID.replaceAll(":", "");
-            //try{pdaID = digest(pdaID);}catch (Exception ex){}
+            Log.e("tmpID.getDeviceId", tmpID);
+        }
+        if (tmpID != null && null != pdaID) {
+            pdaID = tmpID + pdaID;
+        } else if (null == tmpID && null == pdaID) {
+            pdaID = UUID.randomUUID().toString().replaceAll("-", "");
+            Log.e("pdaID.UUID", pdaID);
+        } else if (null == pdaID) {
+            pdaID = tmpID;
         }
         sp.edit().putString("webid", pdaID).apply();
         return pdaID;
